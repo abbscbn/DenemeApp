e076c49e329c735d41763e8a71413b620a298a28
abbscbn <abbscbn@gmail.com>
2020-12-19 18:34:37 +0300

    Yorum satırı eklendi

--- app/src/main/java/com/abbascoban/firstproject/MainActivity.java
+++ app/src/main/java/com/abbascoban/firstproject/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
+// yorum satırı
 public class MainActivity extends AppCompatActivity {
 
     @Override
